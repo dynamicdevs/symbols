9475d34701653847cb92cb098c45270107207052
Milagros O. Almidon <orihuela.ms@gmail.com>
2022-04-11 16:20:46 -0500

    feat: add font family for code

--- apps/web-react/tailwind.config.js
+++ apps/web-react/tailwind.config.js
@@ -27,6 +27,7 @@ module.exports = {
     fontFamily: {
       lato: ['Lato', 'Helvetica', 'Arial', 'sans-serif'],
       montserrat: ['Montserrat', 'Helvetica', 'Arial', 'sans-serif'],
+      inconsolata: ['Inconsolata', 'monospace', 'Arial', 'sans-serif'],
     },
     fontSize: {
       'display-01': [
