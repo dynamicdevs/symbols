41015f126f44b8ec0c8249c340c55775eea98773
Lucho Morales <lmorales@dynamicdevs.io>
2022-05-05 10:55:44 -0500

    refact: update library analytics

--- apps/web-react/src/app/app.tsx
+++ apps/web-react/src/app/app.tsx
@@ -1,7 +1,7 @@
 import Layout from './elements/organisms/Layout';
 import Home from './page/Home';
 
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 const TRACKING_ID = `${process.env['NX_TRACKING_ID']}`;
 ReactGA.initialize(TRACKING_ID);
 
