e1dd9ae27799b8080c3b24ff953c7c981a5c9e3f
Milagros O. Almidon <orihuela.ms@gmail.com>
2022-06-16 10:44:23 -0500

    chore: add children prop on gridwrapper

--- apps/web-react/src/app/elements/organisms/GridWrapper/index.tsx
+++ apps/web-react/src/app/elements/organisms/GridWrapper/index.tsx
@@ -1,18 +1,14 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 
 type Props = {
   className?: string;
+  children: ReactNode;
 };
 
-const GridWrapper: FC<Props> = ({
-  className,
-  children,
-}) => (
+const GridWrapper: FC<Props> = ({ className, children }) => (
   <div className={className}>
     <div className="mx-auto w-[calc(100%-3rem)] md:w-[calc(100%-5rem)] lg:w-[calc(100%-120px)] xl:w-[calc(100%-240px)]">
-      <div className="symbol-grid">
-        {children}
-      </div>
+      <div className="symbol-grid">{children}</div>
     </div>
   </div>
 );
